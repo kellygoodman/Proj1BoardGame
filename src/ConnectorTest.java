bb9abad0d3303f5ff1f8a324a1fea69e7de67926
kellygoodman <kellyg3000@gmail.com>
2013-07-08 16:38:24 -0700

    Updated invalid user input tests

--- src/ConnectorTest.java
+++ src/ConnectorTest.java
@@ -1,66 +1,151 @@
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
-//import org.junit.Test;
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class ConnectorTest extends TestCase{
 
-//	@Test
+public class ConnectorTest {
+
+	@Test
 	public void testConnector() {
 		Connector c1 = new Connector(3,4);
 		assertTrue(c1.endPt1() == 3);
 		assertTrue(c1.endPt2() == 4);
-		assertTrue(c1.toString().equals("34"));
+		assertTrue(c1.toString().equals("34"));		// checks if constructor works when p1 < p2
 		Connector c2 = new Connector(4,3);
-		assertTrue(c1.equals(c2));
+		assertTrue(c1.equals(c2));					// checks if constructor works when p1 > p2
 	}
 
-//	@Test
+	@Test
 	public void testToConnector() {
 		// test toConnector() with valid user inputs
 		Connector c1 = new Connector(3,4);
 		Connector c2 = new Connector(6,1);
-		
-		Connector c3 = Connector.toConnector("34");
-		assertTrue(c1.equals(c3));
-		Connector c4 = Connector.toConnector("43");
-		assertTrue(c1.equals(c4));
-		Connector c5 = Connector.toConnector(" 43");
-		assertTrue(c1.equals(c5));
-		Connector c6 = Connector.toConnector("61");
-		assertTrue(c2.equals(c6));
-		Connector c7 = Connector.toConnector("16 ");
-		assertTrue(c2.equals(c7));
-		Connector c8 = Connector.toConnector("   61  ");
-		assertTrue(c2.equals(c8));
-		
-		
-		//test toConnector() with invalid user inputs
 		boolean thrown = false;
+		Connector c3 = null;
+		
 		try {
-			Connector.toConnector("33");
+			c3 = Connector.toConnector("34");		// p1 < p2		
 		} catch (IllegalFormatException e) {
-		   thrown = true;
+			thrown = true;
+		}
+		assertFalse(thrown);
+		assertTrue(c1.equals(c3));	
+		
+		try {
+			c3 = Connector.toConnector("43");		// p1 > p2
+		} catch (IllegalFormatException e) {
+			thrown = true;
+		}
+		assertFalse(thrown);
+		assertTrue(c1.equals(c3));
+		
+		try {
+			c3 = Connector.toConnector(" 43");		// whitespace before digits
+		} catch (IllegalFormatException e) {
+			thrown = true;
+		}
+		assertFalse(thrown);
+		assertTrue(c1.equals(c3));
+		
+		try {
+			c3 = Connector.toConnector("61");		// digits at the boundary values
+		} catch (IllegalFormatException e) {
+			thrown = true;
+		}
+		assertFalse(thrown);
+		assertTrue(c2.equals(c3));
+		
+		try {
+			c3 = Connector.toConnector("16 ");		// whitespace after digits
+		} catch (IllegalFormatException e) {
+			thrown = true;
+		}
+		assertFalse(thrown);
+		assertTrue(c2.equals(c3));
+		
+		try {
+			c3 = Connector.toConnector("   61   ");	// whitespace before and after digits
+		} catch (IllegalFormatException e) {
+			thrown = true;
+		}
+		assertFalse(thrown);
+		assertTrue(c2.equals(c3));
+		
+		
+		// test toConnector() with invalid user inputs
+		thrown = false;
+		Connector c4 = null;
+		try {
+			c4 = Connector.toConnector("33");	// input with repeated digits
+		} catch (IllegalFormatException e) {
+		    thrown = true;
+		}
+		assertTrue(thrown);
+		assertTrue(c4 == null);
+		
+		thrown = false;
+		try {
+			Connector.toConnector("01");		// input with first digit outside of bounds
+		} catch (IllegalFormatException e) {
+		    thrown = true;
 		}
 		assertTrue(thrown);
 		
-		Connector c9 = Connector.toConnector("33");		
-		assertTrue(c9.equals(null));	
-		Connector c10 = Connector.toConnector("01");
-		assertTrue(c10.equals(null));
-		Connector c11 = Connector.toConnector("47");
-		assertTrue(c11.equals(null));
-		Connector c12 = Connector.toConnector("5");
-		assertTrue(c12.equals(null));
-		Connector c13 = Connector.toConnector("222");
-		assertTrue(c13.equals(null));
-		Connector c14 = Connector.toConnector("");
-		assertTrue(c14.equals(null));
-		Connector c15 = Connector.toConnector("   ");
-		assertTrue(c15.equals(null));
-		Connector c16 = Connector.toConnector("-- ");
-		assertTrue(c16.equals(null));
+		thrown = false;
+		try {
+			Connector.toConnector("47");		// input with second digit outside of bounds
+		} catch (IllegalFormatException e) {
+		    thrown = true;
+		}
+		assertTrue(thrown);
 		
+		thrown = false;
+		try {
+			Connector.toConnector("5");			// input with only one digit
+		} catch (IllegalFormatException e) {
+		    thrown = true;
+		}
+		assertTrue(thrown);
+
+		thrown = false;
+		try {
+			Connector.toConnector("222");		// input with more than 3 digits
+		} catch (IllegalFormatException e) {
+		    thrown = true;
+		}
+		assertTrue(thrown);
+		
+		thrown = false;
+		try {
+			Connector.toConnector("");			// input with empty string
+		} catch (IllegalFormatException e) {
+		    thrown = true;
+		}
+		assertTrue(thrown);
+		
+		thrown = false;
+		try {
+			Connector.toConnector("    ");		// input with only whitespace
+		} catch (IllegalFormatException e) {
+		    thrown = true;
+		}
+		assertTrue(thrown);
+		
+		thrown = false;
+		try {
+			Connector.toConnector("--17");		// input with non-integer characters
+		} catch (IllegalFormatException e) {
+		    thrown = true;
+		}
+		assertTrue(thrown);
+		
+		thrown = false;
+		try {
+			Connector.toConnector("1 2");		// input with whitespace between digits
+		} catch (IllegalFormatException e) {
+		    thrown = true;
+		}
+		assertTrue(thrown);	
 	}
 	
 }
